80b7aa4f452eefd8e331d7bf460e59dfaecb0d77
Yu Mo <ymo@bainainfo.com>
2017-02-23 01:27:12 +0800

    add rnn/chatbot experiment source code

--- rnn/chatbot/chatbot.py
+++ rnn/chatbot/chatbot.py
@@ -2,11 +2,12 @@
 import logging
 import os
 import random
+import re
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.platform import gfile
 from seq2seq_conversation_model import seq2seq_model
-from seq2seq_conversation_model import seq2seq_conversation_model
+from seq2seq_conversation_model import data_utils
+from seq2seq_conversation_model import tokenizer
 from seq2seq_conversation_model.seq2seq_conversation_model import FLAGS, _buckets
 from settings import SEQ2SEQ_MODEL_DIR
 
@@ -20,6 +21,46 @@ UNK_TOKEN_REPLACEMENT = [
 ]
 
 
+ENGLISHWORD_PATTERN = re.compile(r'[a-zA-Z0-9]')
+
+
+def is_unichar_englishnum(char):
+    return ENGLISHWORD_PATTERN.match(char)
+
+
+def trim(s):
+    """
+    1. delete every space between chinese words
+    2. suppress extra spaces
+    :param s: some python string
+    :return: the trimmed string
+    """
+    if not (isinstance(s, unicode) or isinstance(s, str)):
+        return s
+    unistr = s.decode('utf8') if type(s) != unicode else s
+    unistr = unistr.strip()
+    if not unistr:
+        return ''
+    trimmed_str = []
+    if unistr[0] != ' ':
+        trimmed_str.append(unistr[0])
+    for ind in xrange(1, len(unistr) - 1):
+        prev_char = unistr[ind - 1] if len(trimmed_str) == 0 else trimmed_str[-1]
+        cur_char = unistr[ind]
+        maybe_trim = cur_char == ' '
+        next_char = unistr[ind + 1]
+        if not maybe_trim:
+            trimmed_str.append(cur_char)
+        else:
+            if is_unichar_englishnum(prev_char) and is_unichar_englishnum(next_char):
+                trimmed_str.append(cur_char)
+            else:
+                continue
+    if unistr[-1] != ' ':
+        trimmed_str.append(unistr[-1])
+    return ''.join(trimmed_str)
+
+
 class Chatbot():
     """
     answer an enquiry using trained seq2seq model
@@ -30,7 +71,7 @@ class Chatbot():
         self.model = self.create_model(self.session, model_dir, True)
         self.model.batch_size = 1
         # Load vocabularies.
-        vocab_path = os.path.join(seq2seq.FLAGS.data_dir, "vocab%d" % seq2seq.FLAGS.vocab_size)
+        vocab_path = os.path.join(FLAGS.data_dir, "vocab%d" % FLAGS.vocab_size)
         self.vocab, self.rev_vocab = data_utils.initialize_vocabulary(vocab_path)
 
     def create_model(self, session, model_dir, forward_only):
@@ -40,8 +81,9 @@ class Chatbot():
             FLAGS.size, FLAGS.num_layers, FLAGS.max_gradient_norm, FLAGS.batch_size,
             FLAGS.learning_rate, FLAGS.learning_rate_decay_factor, use_lstm=FLAGS.use_lstm,
             forward_only=forward_only)
+
         ckpt = tf.train.get_checkpoint_state(model_dir)
-        if ckpt and gfile.Exists(ckpt.model_checkpoint_path):
+        if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
             _LOGGER.info("Reading model parameters from %s" % ckpt.model_checkpoint_path)
             model.saver.restore(session, ckpt.model_checkpoint_path)
             _LOGGER.info("Read model parameter succeed!")
@@ -56,12 +98,12 @@ class Chatbot():
 
     def generate_answer(self, enquiry):
         # Get token-ids for the input sentence.
-        token_ids = data_utils.sentence_to_token_ids(enquiry, self.vocab, fmm_tokenizer)
+        token_ids = data_utils.sentence_to_token_ids(enquiry, self.vocab, tokenizer.fmm_tokenizer)
         if len(token_ids) == 0:
             _LOGGER.error('lens of token ids of sentence %s is 0' % enquiry)
         # Which bucket does it belong to?
-        bucket_id = min([b for b in xrange(len(seq2seq._buckets))
-                         if seq2seq._buckets[b][0] > len(token_ids)])
+        bucket_id = min([b for b in xrange(len(_buckets))
+                         if _buckets[b][0] > len(token_ids)])
         # Get a 1-element batch to feed the sentence to the model.
         encoder_inputs, decoder_inputs, target_weights = self.model.get_batch(
                 {bucket_id: [(token_ids, [])]}, bucket_id)
@@ -76,9 +118,9 @@ class Chatbot():
 
         # Print out response sentence corresponding to outputs.
         answer = " ".join([self.rev_vocab[output] for output in outputs])
-        if data_utils._UNK in answer:
+        if tokenizer._UNK in answer:
             answer = random.choice(UNK_TOKEN_REPLACEMENT)
-        answer = handler.trim(answer)
+        answer = trim(answer)
         return answer
 
     def close(self):
@@ -86,5 +128,5 @@ class Chatbot():
 
 if __name__ == "__main__":
     m = Chatbot(SEQ2SEQ_MODEL_DIR + '/train/')
-    response = m.generate_answer('我知道你不知道我知道你不知道我说的是什么意思')
+    response = m.generate_answer(u'我知道你不知道我知道你不知道我说的是什么意思')
     print response
