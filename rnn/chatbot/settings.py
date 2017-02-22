a99a4ff959dcb33b69f160118462818c02942f30
Yu Mo <ymo@bainainfo.com>
2017-02-23 01:27:12 +0800

    add rnn/chatbot experiment source code

---
+++ rnn/chatbot/settings.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+from __future__ import print_function, division, absolute_import
+
+VOCAB_DICT_FILE = './data.snippet/vocab100000'
+
+SEQ2SEQ_MODEL_DIR = './data.snippet/'
+SEQ2SEQ_SIZE = 1000
